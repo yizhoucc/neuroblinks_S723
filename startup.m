cfd1091e1e9a467e5d55002c68a3d149e51c9ee5
blinklab <neuroblinks@gmail.com>
2019-01-07 11:13:25 -0600

    Reorganize GUI and file structure
    1. GUI reorganization: got rid of the text boxes for ITI & camera recording time setting that no longer have an effect on the way the trials run (changes from Alvaro before repo initiated). Had to hard-code baseline ITI and camera time options to get the code to start, don't think this should be a problem as long as we maintain the ITI and camera time settings in the trial tabls.
    
    2. Reorganize the file structure: I don't want the neuroblinks config and startup files to be overwritted whenever we pull from the repo. So, I put templates for individual users to customize in the "files you should customize and have in your matlab directory" folder. I put a copy of the correct neuroblinks_config and startup files into my users\oakim\Documents\MATLAB folder so that they will take precedence over these files.

--- startup.m
+++
@@ -1,7 +0,0 @@
-cd 'D:\Data\Greg\Omission'
-
-
-addpath('C:\Users\wojaczyn\Desktop\neuroblinks_left725','C:\Users\wojaczyn\Desktop\neuroblinks_left725\Analysis',...
-        'C:\Users\wojaczyn\Desktop\neuroblinks_left725\Analysis\Ultilities')
-%             ,'C:\greg\matlab')
-gR
